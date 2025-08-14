351af043280c699862c0ed7ac65fc7fbeb1ee1be
kayquesanmartin <kayque.sanmartin65@gmail.com>
2025-08-13 22:07:50 -0300

    feat: delete old folders

--- services/product_service.py
+++
@@ -1,8 +0,0 @@
-from models.product_model import ProductModel
-
-class ProductService:
-    def __init__(self):
-        self.products = ProductModel()
-
-    def create(self, name, price, description):
-        return self.products.add_product(name, price, description).to_dict()
