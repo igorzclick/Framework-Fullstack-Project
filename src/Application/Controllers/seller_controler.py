11a532c5b30bcf7d8db3e0fcf08f812691ab6244
Matheus Abreu <matheus.abreu@duosystem.com.br>
2025-08-25 14:36:21 -0300

    feat: creating request validation

--- src/Application/Controllers/seller_controler.py
+++ src/Application/Controllers/seller_controler.py
@@ -1,20 +1,9 @@
 from flask import request, jsonify, make_response
 from src.Application.Service.seller_service import SellerService
-from apiflask import Schema
-from apiflask.fields import String, Email
-from apiflask.validators import Length
-
-class SellerRegisterSchema(Schema):
-    name = String(required=True, validate=Length(1))
-    cnpj = String(required=True, validate=Length(equal=14))  # Ajuste se quiser permitir formatação
-    email = Email(required=True)
-    cellphone = String(required=True, validate=Length(min=10))
-    password = String(required=True, validate=Length(min=6))
 
 class SellerController:
     @staticmethod
-    def register_seller(body: SellerRegisterSchema):
-
+    def register_seller(body):
         seller = SellerService.create_seller(
             body['name'],
             body['cnpj'],
--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,7 @@
 from src.Application.Controllers.user_controller import UserController
 from src.Application.Controllers.seller_controler import SellerController
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
+from src.Application.Dto.seller_dto import SellerRegisterSchema
 
 def init_routes(app):    
     @app.route('/api', methods=['GET'])
@@ -14,7 +15,8 @@ def init_routes(app):
         return UserController.register_user()
     
     @app.route('/seller', methods=['POST'])
-    def register_seller():
-        return SellerController.register_seller()
+    @app.input(SellerRegisterSchema, location='json')
+    def register_seller(body):
+        return SellerController.register_seller(body)
     
 
