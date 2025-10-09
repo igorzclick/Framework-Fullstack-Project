d4b0865433338a0eb12c369858fc6249eecbe771
sdineijr <sdineijr0@gmail.com>
2025-10-09 17:33:38 -0300

    Add routes and implement tests for each endpoint

---
+++ src/Domain/sale.py
@@ -0,0 +1,14 @@
+class SaleDomain:
+    def __init__(self, seller_id, product_id, quantity, price):
+        self.seller_id = seller_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.price = price
+        
+    def to_dict(self):
+        return {
+            "seller_id": self.seller_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price": self.price
+        }
