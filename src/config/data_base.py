8cfc55310dff8bb13f081b838c1734f14ba72085
sdineijr <sdineijr0@gmail.com>
2025-10-22 20:42:21 -0300

    FIT: Change MYSQL to POSTGRES

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -6,11 +6,12 @@ Base = declarative_base()
 db = SQLAlchemy()
 
 def init_db(app):
-    DB_USER = os.getenv('MYSQL_USER', 'root')
-    DB_PASSWORD = os.getenv('MYSQL_PASSWORD', 'root')
-    DB_HOST = os.getenv('MYSQL_HOST', 'db')
-    DB_NAME = os.getenv('MYSQL_DATABASE', 'marketManagement')
-    SQLALCHEMY_DATABASE_URI = f'mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+    DB_USER = os.getenv('POSTGRES_USER', 'root')
+    DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'root')
+    DB_HOST = os.getenv('POSTGRES_HOST', 'db')
+    DB_PORT = os.getenv('POSTGRES_PORT', '5432')
+    DB_NAME = os.getenv('POSTGRES_DB', 'marketManagement')
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     
     app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
